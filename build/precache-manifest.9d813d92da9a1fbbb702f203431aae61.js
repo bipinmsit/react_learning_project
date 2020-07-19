e0422b0fc242df7b6d3f694c056c6f83554975d9
bipinmsit <bipin.kumar@aspecscire.com>
2020-07-19 15:04:35 +0530

    Create gh-pages branch with static content

---
+++ build/precache-manifest.9d813d92da9a1fbbb702f203431aae61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b1e25e69d3c8526f36cac48ac6c516",
+    "url": "/react_learning_project/index.html"
+  },
+  {
+    "revision": "f9fef51f1b274ca9fb9b",
+    "url": "/react_learning_project/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "4e1a1f5a37475ac54155",
+    "url": "/react_learning_project/static/css/main.61b3ece5.chunk.css"
+  },
+  {
+    "revision": "f9fef51f1b274ca9fb9b",
+    "url": "/react_learning_project/static/js/2.2764259b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_learning_project/static/js/2.2764259b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1a1f5a37475ac54155",
+    "url": "/react_learning_project/static/js/main.0a95490c.chunk.js"
+  },
+  {
+    "revision": "0273a4272e6116b7abdc",
+    "url": "/react_learning_project/static/js/runtime-main.6969ce9a.js"
+  }
+]);
